9455b55a51932c5bd6067513885c5342d6b06f8d
DangCuong1998 <DangCuong1998>
2022-04-06 16:01:45 +0700

    video4

---
+++ video4/video4.js
@@ -0,0 +1,9 @@
+let b='tôi là cường string';
+console.log('b');
+console.log(4+5+'a');
+console.log('a' + 4 +5);
+console.log(4+'a'+5);
+console.log(4+'1'+5);
+
+let c = `i'm iron man`;
+console.log(c)
