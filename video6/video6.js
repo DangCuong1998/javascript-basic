8d54f19f13d4d8d8e2e3d936d8288d3f06fb893b
DangCuong1998 <DangCuong1998>
2022-04-07 11:30:17 +0700

    day bai6,bai7

---
+++ video6/video6.js
@@ -0,0 +1,11 @@
+console.log('hello world from html'.toUpperCase());
+let a =5;
+let b =6;
+let c = 6.9;
+let d = '5';
+
+let e = +d;
+console.log('e value:',e,'type:',typeof e)
+console.log(a-d);
+console.log(a+b);
+console.log(a+c);   
---
+++ video7/video7.js
@@ -0,0 +1,7 @@
+console.log('hello world');
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.');
+console.log(`Hoi dan it check string: a = ${a} b=${b} a+b = ${a +b}` );
+console.log(`Dang quang cuong a=`,a);
