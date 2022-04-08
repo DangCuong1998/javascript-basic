1fa37b9a116f20afa8f30e68d95cfe7e3dabde36
DangCuong1998 <DangCuong1998>
2022-04-08 11:18:44 +0700

    day bai 11

--- video10/video10.js
+++ video10/video10.js
@@ -3,8 +3,9 @@ let a = 5;
 let b ='5';
 console.log('compare a,b','a =',a,'b =',b,'result =',a===b)
 console.log('compare a,b','a =',a,'b =',b,'result =',a==b)
-
+//underfind
 let c;
 console.log('gia tri c',c);
+// empty
 let d ={}
 console.log('hien thi gia tri',d);
---
+++ video11/video11.js
@@ -0,0 +1,7 @@
+for(let i =0;i<10;i++){
+    console.log('check variable:',i)
+}
+let arr =['Mu','arsenal','Mancity','chelsea'];
+for(let i =0;i<arr.length;i++){
+    console.log('check variable i =',arr[i]);
+}
