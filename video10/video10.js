aee089e7dea08408be2a17c3f5e2c51aec9da6c1
DangCuong1998 <DangCuong1998>
2022-04-08 09:54:22 +0700

    day bai 10

---
+++ video10/video10.js
@@ -0,0 +1,10 @@
+console.log('hello wordl with cuong');
+let a = 5;
+let b ='5';
+console.log('compare a,b','a =',a,'b =',b,'result =',a===b)
+console.log('compare a,b','a =',a,'b =',b,'result =',a==b)
+
+let c;
+console.log('gia tri c',c);
+let d ={}
+console.log('hien thi gia tri',d);
