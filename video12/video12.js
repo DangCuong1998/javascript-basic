aba1bd71fee3658e2d52d162baf3e3198979ecf3
DangCuong1998 <DangCuong1998>
2022-04-08 13:20:51 +0700

    day bai  12

---
+++ video12/video12.js
@@ -0,0 +1,12 @@
+console.log('hello world from');
+let arrtop4 = ['Mancity','liverpool','Mu','chelsea']
+
+let i =0
+// while(true){
+//     console.log('Top:',i+1,arrtop4[i]);
+//     i++;
+// }
+
+do{
+
+}
