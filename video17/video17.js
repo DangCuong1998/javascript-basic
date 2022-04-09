89d1206ac9ed5d512cb5f46ba85fb1317a9cdd16
cuongdang <cuongdq62@wru.vn>
2022-04-09 09:56:26 +0700

    day bai 17

---
+++ video17/video17.js
@@ -0,0 +1,12 @@
+console.log('hello world with cuong')
+
+function sum(a,b,i){
+    console.log('1');
+    console.log('2');
+    console.log('3');
+    if(i === 5) return;
+    console.log('4');
+    console.log('5') 
+    return a+b;
+}
+console.log('check sum:',sum(5,6,5));
